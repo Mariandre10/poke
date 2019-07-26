cd3c9ba1b4825bc19263d971c7bd0ac5cb5cd6ff
Maria Hernandez <hermar1001@gmail.com>
2019-07-26 15:35:10 -0400

    Pokemon Model has been created

--- pokapp/models.py
+++ pokapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
+class Pokemon(models.Model):
+    name = models.CharField(max_length=24)
+    specie = models.CharField(max_length=50)
+    photo = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 
